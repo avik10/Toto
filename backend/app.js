94b8c2b8e4af3d8135c31ad534e632c2f3cb1c56
avik10 <bnk.avik@gmail.com>
2024-12-11 02:55:55 +0530

    added db

--- backend/app.js
+++ backend/app.js
@@ -3,11 +3,13 @@ dotenv.config()
 const express = require('express')
 const cors = require('cors')
 const app = express()
+const connectToDB = require('./db/db')
 
+connectToDB()
 
 
 app.get('/', (req, res) => {
-    res.send('Hello World')
+    res.send('Hello Payel, ami onek ta valobasi')
 }); 
 
 module.exports = app;
